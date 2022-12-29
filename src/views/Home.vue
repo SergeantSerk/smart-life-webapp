cbf56f04b36d8a266212950fbe2aaf203fd05487
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2022-12-29 01:17:58 +0000

    Fix future Vue error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,7 @@
     <div v-for="device in devicesSorted" :key="device.id">
       <el-card class="device" :style="device.data.online === false ? 'filter: opacity(0.65) grayscale(1);' : ''">
         <el-tooltip effect="light" :content="device.type" :offset="-20"
-          :visible-arrow="false">
+          :show-arrow="false">
           <el-avatar :src="`/device_icons/${device.type}.png`" shape="square">
             <img src="/device_icons/default.png"/>
           </el-avatar>
